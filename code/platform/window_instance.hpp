f07f5e613e09e663dce093e5f45bb06f5e248f1e
iamdramaticleo <iamdramaticleo@gmail.com>
2024-06-25 16:26:07 +0300

    Rename 'Manager' classes to 'Instance' in platform code
    Renamed 'WindowManager' and 'PlatformManager' classes to 'WindowInstance' and 'PlatformInstance' respectively. Also, updated class references in their functions and included files. The changes reflect a naming scheme more suitable for singularity, indicating the update from management to instance-based logic.

--- code/platform/window_instance.hpp
+++ code/platform/window_instance.hpp
@@ -7,6 +7,12 @@ namespace core
     class WindowInstance
     {
     public:
+        void init(const std::shared_ptr<base::PlatformFactory>& factory);
+        void create(const base::window_config& config); // TODO maybe use the factory in the constructor????
+
+        void release() const;
+        void update()  const;
+
         WindowInstance(const WindowInstance&)            = delete;
         WindowInstance& operator=(const WindowInstance&) = delete;
 
