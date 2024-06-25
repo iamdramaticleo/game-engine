9bf9c0e271971c24b66de961e23a5873d6880821
iamdramaticleo <iamdramaticleo@gmail.com>
2024-06-25 18:08:49 +0300

    Relocate DEPTH_TEST definition and add Pipeline::enable method
    In this change, the GL_DEPTH_TEST definition was moved from the glad.h file to the macros.hpp file under the OpenGL graphics engine's code directory. Additionally, an enable method was added to the Pipeline class within the pipeline.cpp file to enable flags in OpenGL. This commit helps to better organize our OpenGL definitions and enhance functionality.

--- code/graphics/opengl/macros.hpp
+++ code/graphics/opengl/macros.hpp
@@ -5,5 +5,7 @@ namespace gl
     constexpr uint32_t DEPTH_BUFFER_BIT = 0x00000100;
     constexpr uint32_t COLOR_BUFFER_BIT = 0x00004000;
 
+    constexpr uint32_t DEPTH_TEST = 0x0B71;
+
     constexpr uint32_t FALSE = 0;
 }
--- code/graphics/opengl/pipeline.hpp
+++ code/graphics/opengl/pipeline.hpp
@@ -4,5 +4,10 @@ namespace gl
 {
     class Pipeline
     {
+    public:
+        static void enable(uint32_t flag);
+
+    private:
+        Pipeline() = delete;
     };
 }
