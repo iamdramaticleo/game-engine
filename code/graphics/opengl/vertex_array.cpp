1c3b090b06801382a43e0a4157e4513d96febbb7
iamdramaticleo <iamdramaticleo@gmail.com>
2024-06-27 23:49:53 +0300

    Add VertexArray class to OpenGL graphics
    The VertexArray class has been added to the OpenGL graphics section of the engine library. It inherits from the Object class, with the addition of creating, destroying, and binding functions. Also, the inclusion list in the CMakeLists.txt file has been updated accordingly to include this new file.

---
+++ code/graphics/opengl/vertex_array.cpp
@@ -0,0 +1,16 @@
+#include "vertex_array.hpp"
+
+namespace gl
+{
+    void VertexArray::create()
+    {
+    }
+
+    void VertexArray::destroy()
+    {
+    }
+
+    void VertexArray::bind() const
+    {
+    }
+}
