43d9a70e0f0b7c22cd2d789a2cfb1517102e3378
iamdramaticleo <iamdramaticleo@gmail.com>
2024-07-26 23:34:14 +0300

    Add vec3 class implementation to core/math
    Refactored vec3.hpp by moving method definitions to the new vec3.cpp file, enhancing code maintainability. Updated CMakeLists.txt to include vec3.cpp in the build process.

--- code/core/math/vec3.hpp
+++ code/core/math/vec3.hpp
@@ -4,28 +4,11 @@ namespace core::math
 {
     struct vec3
     {
-        vec3 operator*(const float u) const
-        {
-            return { x * u, y * u, z * u };
-        }
-        vec3 normalize()              const
-        {
-            if (const float l = length(); l >= std::numeric_limits<float>::epsilon())
-            {
-                return *this * (1 / l);
-            }
+        vec3 operator*(float u) const;
+        vec3 normalize()        const;
 
-            return *this;
-        }
-
-        float dot(const vec3& a) const
-        {
-            return x * a.x + y * a.y + z * a.z;
-        }
-        float length()           const
-        {
-            return std::sqrtf(dot(*this));
-        }
+        float dot(const vec3& a) const;
+        float length()           const;
 
         float x;
         float y;
