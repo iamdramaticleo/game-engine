624b39bd11ec4240f67b934f7cf26ec36f51ae65
iamdramaticleo <iamdramaticleo@gmail.com>
2024-07-27 00:55:31 +0300

    Refactor buffer_data method names and enhance camera struct
    Renamed buffer_data creation methods for consistency and clarity. Added position attribute to camera struct to better represent its state in 3D space.

--- code/core/data/camera.hpp
+++ code/core/data/camera.hpp
@@ -1,10 +1,14 @@
 #pragma once
 
+#include "math/vec3.hpp"
+
 namespace core::data
 {
     struct camera
     {
-        glm::mat4 view;
-        glm::mat4 proj;
+        glm::mat4  view;
+        glm::mat4  proj;
+
+        math::vec3 position;
     };
 }
